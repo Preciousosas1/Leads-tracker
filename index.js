bd8446060fd2bf365e19bd7ae1335801d078ae72
precious <preciousosazenaye96@gmail.com>
2024-04-29 10:51:22 +0100

    complete...

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let logging = document.getElementById("input-btn");
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+let tabBtn = document.getElementById("tab-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
@@ -42,4 +43,70 @@ deleteBtn.addEventListener("dblclick",function (){
   myLeads = []
   renderLeads()
 })
+
+////////TAB BUTTON
+const tabs = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener ("click", function (){
+  chrome.tabs.query({active:true , currentWindow:true }, function (tabs){
+  myLeads.push(tabs[0].url)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  renderLeads();
+
+
+})
+
+
+})
+
+
+
+
+//refactoring codes
+function getFirst(gty){
+return gty [1]
+}
+let gty = getFirst([12, 7, 8]) 
+
+console.log(gty)
+
+//setting the stage
+
+let player = "per"
+let opponent = "Nick"
+let game = "AmazingFighter"
+const points = 0
+const hasWon = false
+
+
+
+//playing the game
+points+=100
+hasWon = true
+
+
+//announcing the winner
+if (hasWon){
+  console.log(player + " got" + points + 
+  " points and won the" +game + "game" )
+}else{
+  console.log("The winner is" + opponent + "!" 
+  + player + "lost the game")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
